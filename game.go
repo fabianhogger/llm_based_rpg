86e8c844bbabb6cdbb3e220a139871e6c19e8d72
fabian <draizfabian@gmail.com>
2024-12-01 18:04:01 +0100

    basic moving commands

--- game.go
+++ game.go
@@ -2,7 +2,8 @@ package main
 
  
 import (
-  rl "github.com/gen2brain/raylib-go/raylib"
+	  rl "github.com/gen2brain/raylib-go/raylib"
+	  "fmt"
 )
 const (
 	screenWidth = 480
@@ -16,32 +17,97 @@ var (
 	playerSprite rl.Texture2D
 	playersrc rl.Rectangle
 	playerdest rl.Rectangle
+	music rl.Music
+	musicPaused bool
+	playerFramecnt=0
+	Framecount=0
+	playerIsMoving= false
+	playerDir=0
+	val=1
 )
 
 func drawScene(){
 	rl.DrawTexture(grassSprite,100,100,rl.White)
 	rl.DrawTexturePro(playerSprite,playersrc,playerdest, rl.NewVector2(playerdest.Width,	playerdest.Height),1,rl.White)
-
+	/*debugg*/
+	debug_text:=fmt.Sprintf("Framecount, %d,playerframe %d, entered if %d",Framecount,playerFramecnt,val)
+	rl.DrawText(debug_text,150,50,10,rl.White)
+  
 }
 func input(){
+
+
 	if (rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp)){
-		playerdest.Y-=playerSpeed
+		playerIsMoving = true
+		playerDir=1
+		
 	}
 	if (rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown)){
-		playerdest.Y+=playerSpeed
+		playerIsMoving = true
+		playerDir=0
+		
 	}
 	if (rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) ){
-		playerdest.X+=playerSpeed
+		playerIsMoving = true
+		playerDir=3
+		
 	}
 	if (rl.IsKeyDown(rl.KeyL) || rl.IsKeyDown(rl.KeyLeft)){
-		playerdest.X-=playerSpeed
+		playerIsMoving = true
+		playerDir=2
+	
+
 	}
 	if (rl.IsKeyDown(rl.KeyQ)){
 		quit()
 	}
+	if(rl.IsKeyPressed(rl.KeyP)){
+		musicPaused = !musicPaused
+	}
 }
 func update(){
+	rl.UpdateMusicStream(music)
+	rl.ResumeMusicStream(music)
 	running= !rl.WindowShouldClose()
+	playersrc.X=playersrc.Width*float32(playerFramecnt)
+
+	Framecount++
+	if Framecount==65 {Framecount=0}
+	if playerIsMoving{
+	switch(playerDir){
+	case 0:
+		playerdest.Y+=playerSpeed	
+  case 1:
+		playerdest.Y-=playerSpeed	
+  case 2:
+		playerdest.X-=playerSpeed	
+  case 3:
+		playerdest.X+=playerSpeed	
+	}
+	if  Framecount%8==1 { 
+		playerFramecnt++
+}  
+
+
+}
+//idle animation
+	if !playerIsMoving && Framecount%45==1{
+		if 	playerFramecnt==1{
+			playerFramecnt=0
+		}else{
+			playerFramecnt=1
+		} 
+
+}
+
+	
+	if playerFramecnt>3  {
+		playerFramecnt=0
+	}
+	playersrc.X=playersrc.Width*float32(playerFramecnt)
+	playersrc.Y=playersrc.Height*float32(playerDir)
+	playerIsMoving=false
+
 }
 func render(){		
 	rl.BeginDrawing()
@@ -55,9 +121,13 @@ func init(){
 	rl.SetTargetFPS(60)
   grassSprite = rl.LoadTexture("/home/fabian/Documents/GO/SproutLands/SproutLands _ Sprites _ Basicpack/Tilesets/Grass.png")
   playerSprite = rl.LoadTexture("/home/fabian/Documents/GO/SproutLands/SproutLands _ Sprites _ Basicpack/Characters/Basic Charakter Spritesheet.png")
-  playersrc =  rl.NewRectangle(0,0,48, 48)
-  playerdest= rl.NewRectangle(200,200,100,100)
-}	
+  playersrc  =  rl.NewRectangle(0,0,48, 48)
+  playerdest = rl.NewRectangle(200,200,100,100)
+  rl.InitAudioDevice()
+  music=rl.LoadMusicStream("/home/fabian/Documents/GO/SproutLands/SproutLands _ Sprites _ Basicpack/Our-Mountain_v003.mp3")
+  rl.PlayMusicStream(music)
+  musicPaused= false
+}
 func quit(){
 	rl.UnloadTexture(grassSprite)
 	rl.UnloadTexture(playerSprite)
@@ -65,7 +135,6 @@ func quit(){
 }
 
 func main() {
-
 	for running {
 		input()
     update()
---
+++ go.mod
@@ -0,0 +1,10 @@
+module Sprouts
+
+go 1.22.2
+
+require (
+	github.com/ebitengine/purego v0.7.1 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20241114190248-f465c30447d2 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
